65f59aaf048c1a03b52bbe66033a49fc077e193e
Devensh22345 <kumardevensh0@gmail.com>
2024-07-18 14:13:14 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,7 +19,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6950804379:AAEos419_ptoLN0IimtWRQcLy-2M
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-USER_SESSION = os.environ.get("USER_SESSION", "BAFINEQAL797B4mtcyOvRUkhOh3JjYDzWyPrjUGngNiQca8ccKCBZ4zirQPc7456WrtzjQI_thMcykoDDZaQiuNFEI-dW_RJTI-UJfkvCwo8w1KbUEVj658W3CBUSEP_LAR7xMnomKWUjgj3t71la7y-CaLVXsM2qGpOWTqO9fsXNq03fRe561wLujdfeKr2xEXlDUeKlIcBJA-fRlPNoiWQ5IksQvJSFbVZRP1mFqi7ij9HVElYQ9tHAg9wqq99xoJuhPPEo28X09GAXPkXUI3mxmhbAsfq2-suwKbp5kRxQ_4ehev97WYeVdOe401W-4kKsoZ8CU71nyuPYjzH5ABe0feSMgAAAAGmVgSYAA")
+USER_SESSION = os.environ.get("USER_SESSION", "BQBy5STBOLqrAQY4e_NY267m7nwFiuS_7kH8UqGBqsNR1PGr4GqmoTfrfjYxVhPaIiuJFSv1cSsZKGQlR4SJX2zCcMPRyvTXmh-EOS4slOmze9ykrclMwRRE14LrDWvv-uPTM07HZ9fAd0BSc7_yZ2hopatydv5sgAHeamavO6Z_Gdfv_gQoKu7ZP6rqt5-qN4ugnaQ47uJ4bhc1ht-rxZy0iQN9vtD8FMvnQBHpiX-gHh_SEXpqkLch9F5NtiwVJ6PzW09AQnBfDfvNS2V-F3x2qbohwSD4Pfb5XFneLr8b69xUKR3TgeZmuuCZiMtlaFSZJa059uYERcuwCX1uXWAAAAAVUN5igA")
 
 VERIFY = {}
 
