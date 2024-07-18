1594ad1be5db89c2d9fefe33e3b57e34e0e1dd6a
Devensh22345 <kumardevensh0@gmail.com>
2024-07-18 13:59:16 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,7 +19,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6950804379:AAEos419_ptoLN0IimtWRQcLy-2M
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-USER_SESSION = os.environ.get("USER_SESSION", "")
+USER_SESSION = os.environ.get("USER_SESSION", "BAFINEQAL797B4mtcyOvRUkhOh3JjYDzWyPrjUGngNiQca8ccKCBZ4zirQPc7456WrtzjQI_thMcykoDDZaQiuNFEI-dW_RJTI-UJfkvCwo8w1KbUEVj658W3CBUSEP_LAR7xMnomKWUjgj3t71la7y-CaLVXsM2qGpOWTqO9fsXNq03fRe561wLujdfeKr2xEXlDUeKlIcBJA-fRlPNoiWQ5IksQvJSFbVZRP1mFqi7ij9HVElYQ9tHAg9wqq99xoJuhPPEo28X09GAXPkXUI3mxmhbAsfq2-suwKbp5kRxQ_4ehev97WYeVdOe401W-4kKsoZ8CU71nyuPYjzH5ABe0feSMgAAAAGmVgSYAA")
 
 VERIFY = {}
 
